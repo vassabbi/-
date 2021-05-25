d067353546e43738b7c730db884cd8c8217034c5
vassabbi <vassabbi0@gmail.com>
2021-05-26 01:48:30 +0300

    Init Lab5

---
+++ Lab5/Controllers/Controller.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.StaticFiles;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using System.Text;
+
+namespace Lab5.Controllers
+{   
+    [Route("storage")]
+    [ApiController]
+    public class Controller : ControllerBase
+    {
+        [HttpGet("{*path}")]
+        public ActionResult GetFile(string path)
+        {
+            string fullPath = Model.GetFullPath(path);
+            if (Directory.Exists(fullPath))
+            {
+                return new JsonResult(Model.FindAll(fullPath));
+            }
+            else
+            {
+                if (System.IO.File.Exists(fullPath))
+                {
+                    FileInfo info = new FileInfo(fullPath);
+                    string fileType = "application/octet-stream";
+                    return PhysicalFile(fullPath, fileType, info.Name);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+        }
+
+        [HttpDelete("{*path}")]
+        public ActionResult DeleteFile(string path)
+        {
+            string fullPath;
+
+            if (path == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                fullPath = Model.GetFullPath(path);
+            }
+            if (Directory.Exists(fullPath))
+            {
+                Directory.Delete(fullPath);
+                return Ok();
+            }
+            else
+            {
+                if (System.IO.File.Exists(fullPath))
+                {
+                    System.IO.File.Delete(fullPath);
+                    return Ok();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+        }
+
+        [HttpHead("{*path}")]
+        public ActionResult GetFileHeader(string path)
+        {
+            string fullPath = Model.GetFullPath(path);
+            if (Directory.Exists(fullPath))
+            {
+                var info = Model.GetDirInfo(fullPath);
+                foreach (var el in info)
+                {
+                    Response.Headers.Add(el.Key, el.Value);
+                }
+                return Ok();
+            }
+            else
+            {
+                if (System.IO.File.Exists(fullPath))
+                {
+                    var info = Model.GetFileInfo(fullPath);
+                    foreach (var el in info)
+                    {
+                        Response.Headers.Add(el.Key, el.Value);
+                    }
+                    return Ok();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+        }
+        
+        [HttpPut("{*path}")]
+        public ActionResult PutFile(IFormFile file, string path)
+        {
+            if (file != null)
+            {
+                string fullPath = Model.GetFullPath(path);
+                if (Directory.Exists(fullPath))
+                {
+                    try
+                    {
+                        var fileStream = new FileStream(fullPath, FileMode.Create);
+                        file.CopyToAsync(fileStream);
+                        return Ok();
+                    }
+                    catch
+                    {
+                        return BadRequest();
+                    }
+                }
+            }
+            else
+            {
+                return BadRequest();
+            }
+            return BadRequest();
+        }
+    }
+}
---
+++ Lab5/Model.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    public static class Model
+    {
+        private const string Root = @"C:\storage";
+        public static string GetFullPath(string userPath)
+        {
+            if (userPath == null)
+            {
+                return Root;
+            }
+            else
+            {
+                return Path.Combine(Root, userPath);
+            }
+        }
+        public static List<string> FindAll(string path)
+        {
+            var ans = new List<string>();
+            var dirs = Directory.GetDirectories(path);
+            foreach (var directory in dirs)
+            {
+                DirectoryInfo info = new DirectoryInfo(directory);
+                ans.Add(String.Concat("Directory: ", info.Name));
+            }
+            var files = Directory.GetFiles(path);
+            foreach (var file in files)
+            {
+                FileInfo info = new FileInfo(file);
+                ans.Add(String.Concat("File: ", info.Name));
+            }
+            return ans;
+        }
+        public static Dictionary<string, string> GetFileInfo(string path)
+        {
+            Dictionary<string, string> ans = new Dictionary<string, string>();
+            FileInfo info = new FileInfo(path);
+            ans.Add("Directory", info.DirectoryName);
+            ans.Add("Length", info.Length.ToString());
+            ans.Add("Extension", info.Extension);
+            ans.Add("Name", info.Name);
+            ans.Add("Full name", info.FullName);
+            ans.Add("Last change", info.LastWriteTimeUtc.ToString());
+            ans.Add("Was created", info.CreationTimeUtc.ToString());
+            return ans;
+        }
+        public static Dictionary<string, string> GetDirInfo(string path)
+        {
+            Dictionary<string, string> ans = new Dictionary<string, string>();
+            DirectoryInfo info = new DirectoryInfo(path);
+            ans.Add("Name", info.Name);
+            ans.Add("Full name", info.FullName);
+            ans.Add("Last change", info.LastWriteTimeUtc.ToString());
+            ans.Add("Was created", info.CreationTimeUtc.ToString());
+            return ans;
+        }
+    }
+}
